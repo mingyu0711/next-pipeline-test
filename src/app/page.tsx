a161f4a6e469920beb2ddaa608a1af79f0504534
mingyu0711 <minkyu8220@gmail.com>
2024-10-24 15:36:35 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org → ddddd
+          Go to nextjs.org → dddddd
         </a>
       </footer>
     </div>
